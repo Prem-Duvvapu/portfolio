730621fad17225cdbd1529f4606d0a5a5c88ade3
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-07-20 14:12:11 +0000

    about section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Router,
+  Routes,
+  Navigate
+} from "react-router-dom";
 
 function App() {
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    const timer  = setTimeout(() => {
+      setLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,115 @@
+import { Container, Row, Col } from "react-bootstrap";
+import Tilt from "react-parallax-tilt";
+import { AiFillGithub } from "react-icons/ai"
+import { FaLinkedIn } from "react-icons/fa";
+
+const Home2 = () => {
+    return (
+        <Container fluid className="home-about-section" id="about">
+            <Container>
+                <Row>
+                    <Col md={8} className="home-about-description">
+                        <h1 style={{ fontSize: "2.6em "}}>
+                            LET ME <span className="purple"> INTRODUCE </span> MYSELF
+                        </h1>
+
+                        <p className="home-aobut-body">
+                            I fell in love with programming and I havve at least learn something, I think...
+
+                            <br />
+                            <br />
+
+                            I'm fluent in classics like
+                            <i>
+                                <b className="purple">
+                                    Java, Golang, Javascript.
+                                </b>
+                            </i>
+
+                            <br />
+                            <br />
+
+                            My field of Interest's are building new 
+                            <i>
+                                <b className="purple">
+                                    Web Technologies and Products
+                                </b>
+                                and also in areas realted to {" "}
+                                <b className="purple">
+                                    Blockchain.
+                                </b>
+                            </i>
+
+                            <br />
+                            <br />
+
+                            Whenever possible, I also apply my passion for developing Products with
+                            <b className="purple">
+                                Node.js
+                            </b>
+                            and
+                            <i>
+                                <b className="purple">
+                                    {" "}
+                                    Modern Javascript Library and Frameworks
+                                </b>
+                            </i>
+                            like
+                            <i>
+                                <b className="purple">
+                                    React.js
+                                </b>
+                            </i>
+                        </p>
+                    </Col>
+
+                    <Col md={4} className="myAvatar">
+                        <Tilt>
+                            <img src={myImg} className="img-fluid" alt="avatar" />
+                        </Tilt>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md={12} className="home-about-social">
+                        <h1>
+                            FIND ME ON
+                        </h1>
+                        <p>
+                            Feel free to
+                            <span className="purple">
+                                connect
+                            </span>
+                            with me
+                        </p>
+                        <ul className="home-about-social-links">
+                            <li className="social-icons">
+                                <a
+                                    href = "https://github.com/Prem-Duvvapu/"
+                                    target = "_blank"
+                                    rel = "noreferrer"
+                                    className = "icon-color home-social-icons"
+                                >
+                                    <AiFillGithub />
+                                </a>
+                            </li>
+
+                            <li className="social-icons">
+                                <a
+                                    href = "https://www.linkedin.com/in/prem-duvvapu/"
+                                    target = "_blank"
+                                    rel = "noreferrer"
+                                    className = "icon-color home-social-icons"
+                                >
+                                    <FaLinkedIn />
+                                </a>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
+};
+
+export default Home2;
